41251a1c0d11259b836a372cd9fae88084258967
Sn3cor <jakub.krupa23@gmail.com>
2025-11-26 01:31:43 +0100

    Implemented giving out jwt

--- lab_04/app/routes/usersRoutes.js
+++ lab_04/app/routes/usersRoutes.js
@@ -1,13 +1,12 @@
 const express = require('express')
 const { body, validationResult } = require('express-validator')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 const saltRounds = 10
 
 const router = express.Router()
 const User = require('../models/User')
-
-
 
 router.post('/register', body("email").isEmail(), body("password").isLength({ min: 6 }), async (req, res) => {
     const errors = validationResult(req)
@@ -29,6 +28,7 @@ router.post('/register', body("email").isEmail(), body("password").isLength({ mi
                 message: 'User with this email already exists.'
             });
         }
+
         const hash = await bcrypt.hash(password, saltRounds)
         const user = await User.create({ email: email, password: hash })
 
@@ -44,6 +44,45 @@ router.post('/register', body("email").isEmail(), body("password").isLength({ mi
         })
     }
 
+})
+
+router.post('/login', body('email').isEmail(), async (req, res) => {
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty() && errors.errors[0].param === 'email') {
+        return res.status(400).json({ message: 'Invalid email. Try again.' })
+    }
+
+    try {
+        const { email, password } = req.body
+
+        const existingUser = await User.findOne({ where: { email } });
+        if (!existingUser) {
+            return res.status(401).json({
+                message: 'Incorrect credentials.'
+            });
+        }
+
+        const passwordMatch = await bcrypt.compare(password, existingUser.password)
+
+        if (!passwordMatch) {
+            return res.status(401).json({
+                message: 'Incorrect credentials.'
+            })
+        }
+
+        const token = jwt.sign({ id: existingUser.id }, 'private-key', { expiresIn: '1h' })
+        res.status(200).json({
+            message: 'You have successfuly signed in.',
+            token: token
+        })
+
+    } catch (err) {
+        res.status(500).json({
+            message: "Failed to sign in. Try again.",
+            error: err.message
+        })
+    }
 })
 
 module.exports = router
